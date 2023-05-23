705b3dd1be48c29570ebd127234b6f08cce49de4
emetreve <emetreve@gmail.com>
2023-05-23 16:23:28 +0400

    feat: add methods to AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LogInRequest;
 use App\Http\Requests\SignUpRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -12,6 +15,39 @@ class AuthController extends Controller
 	{
 		$data = $request->validated();
 
-		User::create(['name'=>$data['name']]);
+		$user = User::create([
+			'name'    => $data['name'],
+			'email'   => $data['email'],
+			'password'=> bcrypt($data['password']),
+		]);
+
+		$token = $user->createToken('main')->plainTextToken;
+
+		return response(compact('user', 'token'));
+	}
+
+	public function login(LogInRequest $request)
+	{
+		$credentials = $request->validated();
+
+		if (!Auth::attempt($credentials)) {
+			return response([
+				'message'=> 'Provided credentials are incorrect.',
+			]);
+		}
+
+		/** @var User $user */
+		$user = Auth::user();
+		$token = $user->createToken('main')->plainTextToken;
+
+		return response(compact('user', 'token'));
+	}
+
+	public function logout(Request $request)
+	{
+		$user = $request->user();
+		$user->currentAccessToken()->delete();
+
+		return response('', 204);
 	}
 }
