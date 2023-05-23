5818a79564df2cdb5966e9a3088d0d90823b5d3f
emetreve <emetreve@gmail.com>
2023-05-23 19:03:30 +0400

    refactor: remove methods other than signup in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LogInRequest;
 use App\Http\Requests\SignUpRequest;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -25,30 +22,5 @@ class AuthController extends Controller
 		$token = $user->createToken('main')->plainTextToken;
 
 		return response(compact('user', 'token'));
-	}
-
-	public function login(LogInRequest $request)
-	{
-		$credentials = $request->validated();
-
-		if (!Auth::attempt($credentials)) {
-			return response([
-				'message'=> 'Provided credentials are incorrect.',
-			]);
-		}
-
-		/** @var User $user */
-		$user = Auth::user();
-		$token = $user->createToken('main')->plainTextToken;
-
-		return response(compact('user', 'token'));
-	}
-
-	public function logout(Request $request)
-	{
-		$user = $request->user();
-		$user->currentAccessToken()->delete();
-
-		return response('', 204);
 	}
 }
