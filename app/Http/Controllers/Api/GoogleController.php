11be45d56a9f67092b5c9cccd927ea8dfc145948
emetreve <emetreve@gmail.com>
2023-05-30 15:01:33 +0400

    fix: check if user is associated with Google account

--- app/Http/Controllers/Api/GoogleController.php
+++ app/Http/Controllers/Api/GoogleController.php
@@ -21,18 +21,38 @@ class GoogleController extends Controller
 	{
 		$incomingUser = Socialite::driver('google')->stateless()->user();
 
-		$user = User::updateOrCreate([
-			'name' => $incomingUser->name,
-			'email'=> $incomingUser->email,
-		]);
-		if (!$user->email_verified_at) {
+		// $user = User::updateOrCreate([
+		// 	'name'          => $incomingUser->name,
+		// 	'email'         => $incomingUser->email,
+		// 	'is_google_user'=> true,
+		// ]);
+		// if (!$user->email_verified_at) {
+		// 	$user->markEmailAsVerified();
+		// }
+
+		// Auth::login($user);
+		// session()->regenerate();
+
+		// return response()->json(['user'=>$user]);
+		// session()->regenerate();
+
+		$user = User::where('email', $incomingUser->email)->first();
+
+		if (!$user) {
+			$user = User::create([
+				'name'          => $incomingUser->name,
+				'email'         => $incomingUser->email,
+				'is_google_user'=> true,
+			]);
 			$user->markEmailAsVerified();
+			Auth::login($user);
+			return response()->json(['message' => 'User created successfully', 'user' => $user]);
+		} elseif ($user->is_google_user === 1) {
+			Auth::login($user);
+			session()->regenerate();
+			return response()->json(['message' => 'User logged in successfully', 'user' => $user]);
+		} else {
+			return response()->json(['message' => 'User is not a Google user']);
 		}
-
-		Auth::login($user);
-		session()->regenerate();
-
-		return response()->json(['user'=>$user]);
-		session()->regenerate();
 	}
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class() extends Migration {
 			$table->string('password')->nullable();
 			$table->rememberToken();
 			$table->timestamps();
+			$table->boolean('is_google_user')->nullable();
 		});
 	}
 
