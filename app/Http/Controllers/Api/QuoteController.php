a63c010ca7188227e35b5badb788a4d2e5f9b9e8
emetreve <emetreve@gmail.com>
2023-06-13 21:19:21 +0400

    feat: incorporate locale in quotes search

--- app/Http/Controllers/Api/QuoteController.php
+++ app/Http/Controllers/Api/QuoteController.php
@@ -12,28 +12,29 @@ class QuoteController extends Controller
 	public function index(Request $request)
 	{
 		$search = $request->query('search');
+		$locale = $request->query('locale');
 
 		if ($search) {
 			if (Str::startsWith($search, '*')) {
 				$customQuery = Str::substr($search, 1);
 
 				$quotes = Quote::with('movie', 'user', 'comments.user')
-				->where('body', 'like', '%' . $customQuery . '%')
+				->where('body->' . $locale, 'like', '%' . $customQuery . '%')
 				->orderBy('created_at', 'desc')
 				->get();
 			} elseif (Str::startsWith($search, '@')) {
 				$customQuery = Str::substr($search, 1);
 
 				$quotes = Quote::with('movie', 'user', 'comments.user')
-				->whereHas('movie', function ($query) use ($customQuery) {
-					$query->where('name', 'like', '%' . $customQuery . '%');
+				->whereHas('movie', function ($query) use ($customQuery, $locale) {
+					$query->where('name->' . $locale, 'like', '%' . $customQuery . '%');
 				})->orderBy('created_at', 'desc')->get();
 			} else {
 				$quotes = Quote::with('movie', 'user', 'comments.user')
-				->where(function ($query) use ($search) {
-					$query->where('body', 'like', '%' . $search . '%')
-					->orWhereHas('movie', function ($query) use ($search) {
-						$query->where('name', 'like', '%' . $search . '%');
+				->where(function ($query) use ($search, $locale) {
+					$query->where('body->' . $locale, 'like', '%' . $search . '%')
+					->orWhereHas('movie', function ($query) use ($search, $locale) {
+						$query->where('name->' . $locale, 'like', '%' . $search . '%');
 					});
 				})
 			   ->orderBy('created_at', 'desc')
