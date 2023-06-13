c34e3d92b4a7a25f17abae0c81824e6ba4e354d1
emetreve <emetreve@gmail.com>
2023-06-13 13:09:53 +0400

    wip: fintering quotes based on quote/movie content

--- app/Http/Controllers/Api/QuoteController.php
+++ app/Http/Controllers/Api/QuoteController.php
@@ -5,17 +5,40 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Quote;
+use Illuminate\Support\Str;
 
 class QuoteController extends Controller
 {
 	public function index(Request $request)
 	{
 		$search = $request->query('search');
+
 		if ($search) {
-			$quotes = Quote::with('movie', 'user', 'comments.user')
-			->where('body', 'like', '%' . $search . '%')
-			->orderBy('created_at', 'desc')
-			->get();
+			if (Str::startsWith($search, '#')) {
+				$customQuery = Str::substr($search, 1);
+
+				$quotes = Quote::with('movie', 'user', 'comments.user')
+				->where('body', 'like', '%' . $customQuery . '%')
+				->orderBy('created_at', 'desc')
+				->get();
+			} elseif (Str::startsWith($search, '@')) {
+				$customQuery = Str::substr($search, 1);
+
+				$quotes = Quote::with('movie', 'user', 'comments.user')
+				->whereHas('movie', function ($query) use ($customQuery) {
+					$query->where('name', 'like', '%' . $customQuery . '%');
+				})->orderBy('created_at', 'desc')->get();
+			} else {
+				$quotes = Quote::with('movie', 'user', 'comments.user')
+				->where(function ($query) use ($search) {
+					$query->where('body', 'like', '%' . $search . '%')
+					->orWhereHas('movie', function ($query) use ($search) {
+						$query->where('name', 'like', '%' . $search . '%');
+					});
+				})
+			   ->orderBy('created_at', 'desc')
+			   ->get();
+			}
 		} else {
 			$quotes = Quote::with('movie', 'user', 'comments.user')->orderBy('created_at', 'desc')->get();
 		}
