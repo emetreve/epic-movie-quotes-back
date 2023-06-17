9ae9c4cd606542c5c9278d6064be7c1b70b5a551
emetreve <emetreve@gmail.com>
2023-06-17 15:09:03 +0400

    fix: PrivateChannel argument

--- app/Events/NotificationUpdated.php
+++ app/Events/NotificationUpdated.php
@@ -2,9 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -12,27 +10,27 @@ use Illuminate\Queue\SerializesModels;
 
 class NotificationUpdated implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+	use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $message;
+	public $message;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct($message)
+	/**
+	 * Create a new event instance.
+	 */
+	public function __construct($message)
 	{
 		$this->message = $message;
 	}
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('notification-updated', $this->message->end_user_id),
-        ];
-    }
+	/**
+	 * Get the channels the event should broadcast on.
+	 *
+	 * @return array<int, \Illuminate\Broadcasting\Channel>
+	 */
+	public function broadcastOn(): array
+	{
+		return [
+			new PrivateChannel('notification-updated.' . $this->message->end_user_id),
+		];
+	}
 }
