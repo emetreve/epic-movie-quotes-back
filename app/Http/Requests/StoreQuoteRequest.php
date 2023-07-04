923e1d930ece27e2f330e8831079474602bdc356
emetreve <emetreve@gmail.com>
2023-07-04 11:56:10 +0400

    feat: translate and return errors from create quote

--- app/Http/Requests/StoreQuoteRequest.php
+++ app/Http/Requests/StoreQuoteRequest.php
@@ -6,6 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreQuoteRequest extends FormRequest
 {
+	protected function prepareForValidation()
+	{
+		$locale = $this->input('locale');
+		app()->setLocale($locale);
+
+		parent::prepareForValidation();
+	}
+
 	/**
 	 * Get the validation rules that apply to the request.
 	 *
---
+++ lang/ka/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'mimes'      => ':attribute უნდა იყოს შემდეგი ფორმატის: :values.',
+	'required'   => ':attribute სავალდებულოა.',
+	'attributes' => [
+		'image'     => 'სურათი',
+		'bodyEn'    => 'ინგლისური ტექსტი',
+		'bodyGe'    => 'ქართული ტექსტი',
+	],
+];
