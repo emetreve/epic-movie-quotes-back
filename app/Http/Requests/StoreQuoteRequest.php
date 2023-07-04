2e54c20010cda794346f853a9090e4a379ed361c
emetreve <emetreve@gmail.com>
2023-07-04 12:38:40 +0400

    refactor: remove comments

--- app/Http/Requests/StoreQuoteRequest.php
+++ app/Http/Requests/StoreQuoteRequest.php
@@ -14,11 +14,6 @@ class StoreQuoteRequest extends FormRequest
 		parent::prepareForValidation();
 	}
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-	 */
 	public function rules(): array
 	{
 		return [
